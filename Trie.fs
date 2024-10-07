8c03ccf725e290205ac0f6d9032042f73ae9a7a7
snOlga <olga.safonova@outlook.com>
2024-10-07 18:54:13 +0300

    working on inserting

--- Trie.fs
+++ Trie.fs
@@ -5,8 +5,8 @@ type Node = { Value: char; Children: seq<Node> }
 let create = { Value = ' '; Children = Seq.empty }
 
 let rec insert (word: char seq) (current: Node) : Node =
-    match Seq.length current.Children with
-    | 0 when current.Value = ' ' ->
+    match Seq.length current.Children with // is children exsit
+    | 0 when current.Value = ' ' -> // to root
         let newChild =
             insert
                 (word)
@@ -14,11 +14,11 @@ let rec insert (word: char seq) (current: Node) : Node =
                   Children = Seq.empty }
 
         { current with Children = (Seq.singleton (newChild)) }
-    | 0 when Seq.length word = 1 ->
+    | 0 when Seq.length word = 1 -> // last char as leaf
         { current with
             Value = Seq.head word
             Children = Seq.empty }
-    | 0 when Seq.length word > 1 ->
+    | 0 when Seq.length word > 1 -> // part of word as "leaf"
         let newChild =
             insert
                 (Seq.tail word)
@@ -28,6 +28,35 @@ let rec insert (word: char seq) (current: Node) : Node =
         { Value = Seq.head word
           Children = Seq.append current.Children (Seq.singleton (newChild)) }
     | _ ->
-        { Value = Seq.head word
+        match Seq.tryFind (fun node -> node.Value = (Seq.head word)) current.Children with
+        | Some child when Seq.length word <> 1 ->
+            let updatedChild = insert (Seq.tail word) child
+
+            { current with
+                Children =
+                    Seq.map
+                        (fun node ->
+                            if node.Value = (Seq.head word) then
+                                updatedChild
+                            else
+                                node)
+                        current.Children }
+        | Some child when Seq.length word = 1 -> current
+        | None when Seq.length word <> 1 ->
+            let newChild =
+                { Value = Seq.head word
+                  Children =
+                    Seq.singleton (
+                        insert
+                            (Seq.tail word)
+                            { Value = (Seq.head word)
+                              Children = Seq.empty }
+                    ) }
+
+            { current with Children = Seq.append current.Children (Seq.singleton newChild) }
+        | None when Seq.length word = 1 ->
+            let newChild =
+                { Value = (Seq.head word)
                   Children = Seq.empty }
-        //TODO: 
+
+            { current with Children = Seq.append current.Children (Seq.singleton newChild) }
--- UnitTest1.fs
+++ UnitTest1.fs
@@ -4,26 +4,170 @@ open NUnit.Framework
 open Trie
 
 [<Test>]
-let insertToEmptyTest () =
-    Assert.AreEqual ( {
-        Value = ' '
-        Children =
-        seq {
-            yield {
-                Value = 'a'
-                Children = 
-                seq {
-                    yield {
-                        Value = 'b'
-                        Children = 
+let insertTest1 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children =
+            seq {
+                yield
+                    { Value = 'a'
+                      Children =
                         seq {
-                            yield {
-                                Value = 'c'
-                                Children = Seq.empty
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    } , create |> insert "abc")
+                            yield
+                                { Value = 'b'
+                                  Children = seq { yield { Value = 'c'; Children = Seq.empty } } }
+                        } }
+            } },
+        create |> insert "abc"
+    )
+
+[<Test>]
+let insertTest2 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children =
+            seq {
+                yield { Value = 'a'; Children = Seq.empty }
+                yield { Value = 'b'; Children = Seq.empty }
+            } },
+        create |> insert "a" |> insert "b"
+    )
+
+[<Test>]
+let insertTest3 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children =
+            seq {
+                yield
+                    { Value = 'a'
+                      Children =
+                        seq {
+                            yield
+                                { Value = 'b'
+                                  Children = Seq.empty
+
+                                }
+                        } }
+
+                yield { Value = 'b'; Children = Seq.empty }
+            } },
+        create |> insert "ab" |> insert "b"
+    )
+
+[<Test>]
+let insertTest4 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children =
+            seq {
+                yield
+                    { Value = 'a'
+                      Children =
+                        seq {
+                            yield { Value = 'b'; Children = Seq.empty }
+                            yield { Value = 'c'; Children = Seq.empty }
+                        } }
+            } },
+        create |> insert "ab" |> insert "ac"
+    )
+
+[<Test>]
+let insertTest5 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children =
+            seq {
+                yield
+                    { Value = 'a'
+                      Children = seq { yield { Value = 'b'; Children = Seq.empty } } }
+            } },
+        create |> insert "ab" |> insert "a"
+    )
+
+[<Test>]
+let insertTest6 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children = seq { yield { Value = 'a'; Children = Seq.empty } } },
+        create |> insert "a" |> insert "a"
+    )
+
+[<Test>]
+let insertTest7 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children =
+            seq {
+                yield
+                    { Value = 'a'
+                      Children = seq { yield { Value = 'b'; Children = Seq.empty } } }
+
+                yield { Value = 'c'; Children = Seq.empty }
+            } },
+        create |> insert "ab" |> insert "c"
+    )
+
+[<Test>]
+let insertTest8 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children =
+            seq {
+                yield
+                    { Value = 'a'
+                      Children = seq { yield { Value = 'b'; Children = Seq.empty } } }
+
+                yield
+                    { Value = 'c'
+                      Children = seq { yield { Value = 'a'; Children = Seq.empty } } }
+            } },
+        create |> insert "ab" |> insert "ca"
+    )
+
+[<Test>]
+let insertTest9 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children =
+            seq {
+                yield
+                    { Value = 'a'
+                      Children =
+                        seq {
+                            yield
+                                { Value = 'a'
+                                  Children = seq { yield { Value = 'a'; Children = Seq.empty } } }
+                        } }
+            } },
+        create |> insert "aaa"
+    )
+
+[<Test>]
+let insertTest10 () =
+    Assert.AreEqual(
+        { Value = ' '
+          Children =
+            seq {
+                yield
+                    { Value = 'a'
+                      Children =
+                        seq {
+                            yield
+                                { Value = 'b'
+                                  Children =
+                                    seq {
+                                        yield { Value = 'q'; Children = Seq.empty }
+                                        yield { Value = 'c'; Children = Seq.empty }
+                                    } }
+
+                            yield
+                                { Value = 'd'
+                                  Children = seq { yield { Value = 'k'; Children = Seq.empty } } }
+                        } }
+            } },
+        create
+        |> insert "abq"
+        |> insert "abc"
+        |> insert "adk"
+    )
